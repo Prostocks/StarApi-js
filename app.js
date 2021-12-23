ed8786dbe6f3412c3d14126f7567bfba5c06ea1a
kumar.anand <kumar.anand@kambala.co.in>
2021-12-23 14:00:30 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const Api = require("./lib/NorenRestApi");
+
+let { authparams } = require("./cred");
+
+api = new Api({});
+
+function receiveQuote(data) {
+    console.log("Quote ::", data);
+}
+
+function receiveOrders(data) {
+    console.log("Order ::", data);
+}
+
+function open(data) {
+    let instruments = 'NSE|22#BSE|500400';
+    api.subscribe(instruments)
+    console.log("subsribing to :: ", instruments);
+}
+
+
+
+api.login(authparams)
+.then((res) => {        
+        //cons//ole.log('Reply: ', res);
+        params = {
+          'socket_open' : open,
+          'quote' : receiveQuote,   
+          'order' : receiveOrders       
+        }
+
+        api.start_websocket(params);
+
+        //search scrip example
+        api.searchscrip('NFO', 'NIFTY DEC CE').then((reply) => { console.log(reply); });
+        
+        //get quote example
+        api.get_quotes('NSE', '22').then((reply) => { console.log(reply); });
+
+        api.get_orderbook().then((reply) => { console.log(reply); });
+
+        api.get_tradebook().then((reply) => { console.log(reply); });
+
+        api.get_holdings().then((reply) => { console.log(reply); });
+        
+        api.get_positions().then((reply) => { console.log(reply); });
+        
+    }).catch((err) => {
+        console.error(err);
+    });
+
